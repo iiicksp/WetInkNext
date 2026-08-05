548f1a94b34b2fcc7474c18acd4108b49fecb6c3
iiicksp <iiicksp@gmail.com>
2026-08-05 21:07:00 +0300

    fix(p3): place GLSL version directive first

--- app/src/main/java/com/wetinknext/engine/gl/ShaderLib.kt
+++ app/src/main/java/com/wetinknext/engine/gl/ShaderLib.kt
@@ -1,8 +1,7 @@
 package com.wetinknext.engine.gl
 
 object ShaderLib {
-    const val canvasPresentVertex = """
-        #version 300 es
+    const val canvasPresentVertex = """#version 300 es
         layout(location = 0) in vec2 aPosition;
         uniform mat4 uCanvasToClip;
         uniform vec2 uCanvasSize;
@@ -12,22 +11,19 @@ object ShaderLib {
             gl_Position = uCanvasToClip * vec4(aPosition, 0.0, 1.0);
         }
     """
-    const val presentFragment = """
-        #version 300 es
+    const val presentFragment = """#version 300 es
         precision highp float;
         in vec2 vUv;
         uniform sampler2D uTexture;
         out vec4 fragColor;
         void main() { fragColor = texture(uTexture, vUv); }
     """
-    const val fullscreenVertex = """
-        #version 300 es
+    const val fullscreenVertex = """#version 300 es
         layout(location = 0) in vec2 aPosition;
         out vec2 vUv;
         void main() { vUv = aPosition * 0.5 + 0.5; gl_Position = vec4(aPosition, 0.0, 1.0); }
     """
-    const val solidFragment = """
-        #version 300 es
+    const val solidFragment = """#version 300 es
         precision highp float;
         in vec2 vUv;
         out vec4 fragColor;
