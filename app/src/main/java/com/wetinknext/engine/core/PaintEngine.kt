47f14b4e4a78d4ea45e21faaaa86e26c438fc50d
iiicksp <iiicksp@gmail.com>
2026-08-05 21:04:12 +0300

    feat(p3): add paint engine document target

---
+++ app/src/main/java/com/wetinknext/engine/core/PaintEngine.kt
@@ -0,0 +1,34 @@
+package com.wetinknext.engine.core
+
+import com.wetinknext.engine.gl.GlCaps
+import com.wetinknext.engine.gl.RenderTarget
+
+/** Document state that is exclusively owned by the GLSurfaceView render thread. */
+class PaintEngine {
+    val camera = Camera()
+    val canvasTarget = RenderTarget()
+    var canvasWidth = 1; private set
+    var canvasHeight = 1; private set
+    var initialized = false; private set
+
+    fun create(caps: GlCaps, width: Int, height: Int) {
+        require(width > 0 && height > 0)
+        release()
+        canvasWidth = width; canvasHeight = height
+        canvasTarget.create(width, height, caps.supportsHalfFloatColorBuffer)
+        initialized = true
+    }
+
+    fun resize(caps: GlCaps, width: Int, height: Int) {
+        if (!initialized || canvasWidth != width || canvasHeight != height) create(caps, width, height)
+    }
+
+    fun clearCanvas() {
+        check(initialized)
+        canvasTarget.clear(1f, 1f, 1f, 1f)
+    }
+
+    fun release() {
+        canvasTarget.release(); initialized = false; canvasWidth = 1; canvasHeight = 1
+    }
+}
